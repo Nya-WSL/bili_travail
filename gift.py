83632dec38ae7e15eb731371bcc1569be536e49a
狐日泽 <shdocter@nya-wsl.com>
2025-01-25 11:47:14 +0800

    v0.14.0-beta
    - 适配爬虫

--- gift.py
+++ gift.py
@@ -1,82 +1,110 @@
 import os
 import json
+import requests
+from blive_crower import get_bili_h5
 from typing import Union
 from bs4 import BeautifulSoup
 
-def get_gift(path: str, h5_path = "example.htm", show = False, write = True, time: Union[int, float] = 0, write_time = False, return_dict = False):
+def get_gift(room_id, path: str, json_path = "data/gift_img.json", h5_path = "data/saved_page.html", show = False, write = True, time: Union[int, float] = 0, write_time = False, return_dict = False, init = False):
     """
-    解析B站直播间礼物标签和URL，并保存为JSON文件
+    爬取并解析B站直播间礼物标签和URL，保存为JSON文件
+    :param room_id: 房间号
     :param path: 保存JSON文件的路径
+    :param json_path: 服务器预留的json文件的路径
     :param h5_path: 需解析的HTML文件的路径
     :param show: 是否打印解析结果
     :param write: 是否将解析结果保存为JSON文件
     :param time: 礼物时长
     :param write_time: 是否将礼物时长保存为JSON文件
     :param return_dict: 是否返回解析结果的字典
+    :param init: 是否进入初始化循环
     """
 
-    # 打开并读取 HTML 文件内容
-    with open(h5_path, "r", encoding="utf-8") as file:
-        html_content = file.read()
-
-    # 使用BeautifulSoup解析HTML
-    soup = BeautifulSoup(html_content, 'html.parser')
-
-    # 创建一个空字典来存储礼物标签和URL
-    gift_mapping = {}
-
-    # 找到所有的礼物项
-    gift_items = soup.find_all('div', class_='gift-item')
-
-    # 遍历每个礼物项，提取标签和URL
-    for item in gift_items:
-
-        # 获取图像URL
-        img_div = item.find('div', class_='img')
-        if img_div:  # 确保img_div存在
-            img_url = img_div['style']
-            img_url = img_url.split('url("')[1].split(')')[0].strip("'").replace('webp"', "webp")
-        else:
-            img_url = "error"
-
-        # 获取礼物标签
-        gift_tag = item.find('p', class_='gift-label')
-        if gift_tag:  # 确保gift_tag存在
-            gift_name = gift_tag.text.strip()
-        else:
-            gift_name = "error"
-
-        # 将标签和URL对应起来
-        gift_mapping[gift_name] = img_url
-        gift_mapping.pop('error', "None")
-
-        guard = {
-            "舰长": "guard-level-3.png",
-            "提督": "guard-level-2.png",
-            "总督": "guard-level-1.png"
-        }
-        url = "https://nya-wsl.com/images/bili_travail/"
-        for k,v in guard.items():
-            if not os.path.exists(f"data/{k}"):
-                gift_mapping[k] = url + v
+    if init:
+        url = "https://nya-wsl.com/bili_travail/gift/gift_img.json"
+        try:
+            get_basic_gift = requests.get(url)
+            if get_basic_gift.status_code == 200:
+                print("[INIT] 成功获取Nya-WSL服务器存档数据...")
+                with open(json_path, "w+", encoding="utf-8") as f:
+                    json.dump(get_basic_gift.json(), f, ensure_ascii=False, indent=4) # 从服务器拉取返回的json数据并写入
             else:
-                gift_mapping[k] = f"data/{v}"
+                raise ValueError("[ERROR] 无法获取Nya-WSL服务器存档数据...")
+        except:
+            print("[ERROR] 联网获取礼物数据失败...")
+            print("[INIT] 尝试重构基础礼物数据...")
+            import gift_mapping as gift_map
+            with open(json_path, "w", encoding="utf-8") as f:
+                json.dump(gift_map.gift_mapping, f, ensure_ascii=False, indent=4)
+            print("[INIT] 数据已重构为基础预设...")
+    else:
+        # 使用blive_crower爬取所连接的直播间h5代码
+        is_html = False
+        html_content = get_bili_h5(room_id, json_path, h5_path)
+        if html_content: # 如果返回的是return_json_status = True
+            with open(json_path, "r", encoding="utf-8") as f:
+                gift_mapping = json.load(f)
+        else:
+            is_html = True
+            with open(h5_path, "r", encoding="utf-8") as f:
+                html_content = f.read()
 
-    if show:
-        # 打印映射结果
-        for url, name in gift_mapping.items():
-            print(f"URL: {url} -> 标签: {name}")
-    if write:
-        # 写入json文件
-        with open(path, "w", encoding="utf-8") as file:
-            json.dump(gift_mapping, file, ensure_ascii=False, indent=4)
-    if write_time:
-        tmp_dict = {}
-        for k,v in gift_mapping.items():
-            tmp_dict[k] = time
-        # 写入json文件
-        with open(path, "w", encoding="utf-8") as file:
-            json.dump(tmp_dict, file, ensure_ascii=False, indent=4)
-    if return_dict:
-        # 返回字典
-        return gift_mapping
+        if is_html:
+            soup = BeautifulSoup(html_content, 'html.parser') # 使用BeautifulSoup解析HTML
+
+            gift_mapping = {} # 创建一个空字典来存储礼物标签和URL
+
+            gift_items = soup.find_all('div', class_='gift-item') # 找到所有的礼物项
+
+            for item in gift_items: # 遍历每个礼物项，提取标签和URL
+
+                # 获取图像URL
+                img_div = item.find('div', class_='img')
+                if img_div:  # 确保img_div存在
+                    img_url = img_div['style']
+                    img_url = img_url.split('url("')[1].split(')')[0].strip("'").replace('webp"', "webp")
+                else:
+                    img_url = "error"
+
+                # 获取礼物标签
+                gift_tag = item.find('p', class_='gift-label')
+                if gift_tag:  # 确保gift_tag存在
+                    gift_name = gift_tag.text.strip()
+                else:
+                    gift_name = "error"
+
+                # 将标签和URL对应起来
+                gift_mapping[gift_name] = img_url
+                gift_mapping.pop('error', "None")
+
+                guard = {
+                    "舰长": "guard-level-3.png",
+                    "提督": "guard-level-2.png",
+                    "总督": "guard-level-1.png"
+                }
+                url = "https://nya-wsl.com/images/bili_travail/"
+                for k,v in guard.items():
+                    if not os.path.exists(f"data/{k}"):
+                        gift_mapping[k] = url + v
+                    else:
+                        gift_mapping[k] = f"data/{v}"
+
+        if show:
+            # 打印映射结果
+            for url, name in gift_mapping.items():
+                print(f"URL: {url} -> 标签: {name}")
+        if write:
+            # 写入json文件
+            with open(path, "w", encoding="utf-8") as file:
+                json.dump(gift_mapping, file, ensure_ascii=False, indent=4)
+            os.remove(h5_path)
+        if write_time:
+            tmp_dict = {}
+            for k,v in gift_mapping.items():
+                tmp_dict[k] = time
+            # 写入json文件
+            with open(path, "w", encoding="utf-8") as file:
+                json.dump(tmp_dict, file, ensure_ascii=False, indent=4)
+        if return_dict:
+            # 返回字典
+            return gift_mapping
