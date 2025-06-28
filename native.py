db40d7f011a63769dc1f89eef1c0c14e059a2a8d
狐日泽 <shdocter@nya-wsl.com>
2025-06-29 05:52:39 +0800

    v0.27.2-fix1
    - B站弹幕服务器web接口的User-Agent疑似被风控，更换User-Agent

--- native.py
+++ native.py
@@ -25,7 +25,7 @@ import browser_cookie3
 from typing import *
 from nicegui import ui, app
 
-version = "0.27.2-fix"
+version = "0.27.2-fix1"
 logger.debug("version: {}", version)
 
 # ================================
