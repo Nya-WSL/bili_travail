e35ded717fda739ba215c506642e7f37c4c65483
狐日泽 <shdocter@nya-wsl.com>
2026-02-23 01:56:25 +0800

    修复当小版本的第一位为0时会打包错误的问题

--- build.py
+++ build.py
@@ -33,7 +33,7 @@ def create_version():
 
     # 使用正则表达式替换版本
     pattern = r'"version":\s*(\d+)'
-    replacement = f'"version": {version}'
+    replacement = f'"version": "{version}"'
     new_content = re.sub(pattern, replacement, content)
 
     with open("env.py", 'w', encoding='utf-8') as f:
