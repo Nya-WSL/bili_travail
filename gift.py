2e22e03c530ff8486e27f4de2861e7bb84e1128e
狐日泽 <shdocter@nya-wsl.com>
2026-03-22 08:56:14 +0800

    重新等待下发自定义礼物数据增加重试上限避免无限递归

--- gift.py
+++ gift.py
@@ -22,6 +22,7 @@ class BiliGiftManager:
         self.area_parent_id = 0
         self.area_id = 0
         self.custom_gifts = []
+        self.wait_num = 0
 
     async def init_gift(self, img_path):
         """
@@ -141,7 +142,8 @@ class BiliGiftManager:
                 if response.status == 200:
                     data = await response.json()
                     if data["code"] == 0:
-                        if len(data["data"]["gift_config"]["room_config"]) == 1:
+                        if len(data["data"]["gift_config"]["room_config"]) == 1 and self.wait_num < 3: # 只有一个值一般是红包，大概率还未下发自定义礼物数据，继续等待，最多等待3次避免无限递归
+                            self.wait_num += 1
                             logger.warning(f"房间{self.room_id}的自定义礼物数据可能还未下发，继续等待...")
                             await asyncio.sleep(5)
                             await self.get_room_gift(platform)
