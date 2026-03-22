91e2bd9914109005cccb3677fc38b0e34784b7f8
狐日泽 <shdocter@nya-wsl.com>
2026-03-22 08:30:56 +0800

    调整暴击的数值显示，现在会显示最终时长

--- main.py
+++ main.py
@@ -1806,7 +1806,7 @@ async def capture():
             gift_img_avatar.set_source(gift_img.get(k, ""))
             k_label.set_text(k)
             if k in GiftManager.custom_gifts:
-                v_label.set_text(f"{format_seconds(v)} 暴击{format_seconds(v * abs(1 - app.storage.general['custom_gift_rate'][k]))}")
+                v_label.set_text(f"{format_seconds(v)} 暴击{format_seconds(v * abs(1 - app.storage.general['custom_gift_rate'][k]) + v)}")
             else:
                 v_label.set_text(format_seconds(v))
             k_label.classes(replace="text-3xl font-extrabold")
@@ -1883,7 +1883,7 @@ async def capture():
                 k_label = ui.label(k).classes("text-3xl font-extrabold").style(f"color: {config["color"]['text_color']}")
                 ui.space()
                 if k in GiftManager.custom_gifts:
-                    v_label = ui.label(f"{format_seconds(v)} 暴击{format_seconds(v * abs(1 - app.storage.general['custom_gift_rate'][k]))}").classes("text-3xl font-extrabold").style(f"color: {config["color"]['text_color']}")
+                    v_label = ui.label(f"{format_seconds(v)} 暴击{format_seconds(v * abs(1 - app.storage.general['custom_gift_rate'][k]) + v)}").classes("text-3xl font-extrabold").style(f"color: {config["color"]['text_color']}")
                 else:
                     v_label = ui.label(format_seconds(v)).classes("text-3xl font-extrabold").style(f"color: {config["color"]['text_color']}")
 
