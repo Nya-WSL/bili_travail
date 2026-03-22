8a151ba68dfd3fdbd21aeaad70f45ff62ef8d98a
狐日泽 <shdocter@nya-wsl.com>
2026-03-22 08:41:29 +0800

    连接弹幕服务器时B站可能不会在5秒内下发自定义礼物数据，现在没有检测到数据时会继续等待

--- gift.py
+++ gift.py
@@ -1,7 +1,9 @@
 import os
 import re
-import aiohttp
 import orjson
+import asyncio
+import aiohttp
+
 import gift_mapping as gift_map
 
 import dns_resolver
@@ -139,6 +141,10 @@ class BiliGiftManager:
                 if response.status == 200:
                     data = await response.json()
                     if data["code"] == 0:
+                        if len(data["data"]["gift_config"]["room_config"]) == 1:
+                            logger.warning(f"房间{self.room_id}的自定义礼物数据可能还未下发，继续等待...")
+                            await asyncio.sleep(5)
+                            await self.get_room_gift(platform)
                         for gift in data["data"]["gift_config"]["room_config"]:
                             if gift["corner_mark"] == "玩法":
                                 if gift["name"] not in self.custom_gifts:
