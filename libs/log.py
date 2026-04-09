11e5d49248fcb14c966ddb0917a559eca5e107c9
狐日泽 <shdocter@nya-wsl.com>
2026-04-10 00:55:29 +0800

    记录日志前尝试对手机号和座机号脱敏

--- libs/log.py
+++ libs/log.py
@@ -1,4 +1,5 @@
 import os
+import re
 import sys
 import datetime
 from loguru import logger
@@ -13,10 +14,25 @@ file_name = os.path.join("logs", f"bili_travail_{file_time}.log")
 
 
 def mask_home_dir(record):
-    """将日志中的 HOME_DIR 替换为 ~"""
+    """将日志中的 HOME_DIR 脱敏"""
     msg = record.get("message", "")
     record["message"] = msg.replace(HOME_DIR, "[HOME_DIR]")
 
+    return record
+
+
+def mask_phone_num(record):
+    """将日志中疑似电话的信息脱敏"""
+    msg = record.get("message", "")
+    record["message"] = re.sub(r"1[3-9]\d{9}", "*" * 11, msg)
+    record["message"] = re.sub(r"0[0-9]\d{9}", "*" * 11, record["message"])
+    record["message"] = re.sub(r"\d{3}-\d{4}-\d{4}", "***-****-****", record["message"])
+    record["message"] = re.sub(r"\d{3} \d{4} \d{4}", "*** **** ****", record["message"])
+    record["message"] = re.sub(r"\d{4} \d{4}", "**** ****", record["message"])
+    record["message"] = re.sub(r"\d{3}-\d{8}", "***-********", record["message"])
+    record["message"] = re.sub(r"\d{4}-\d{4}", "****-****", record["message"])
+
+    return record
 
 logger.remove()  # 移除默认 handler
 
@@ -29,8 +45,14 @@ logger.add(
     format="{time:%Y-%m-%d %H:%M:%S} [{level}]: {name} | {function}({line}): <level>{message}</level>",
 )
 
-logger = logger.patch(mask_home_dir)
+def mask_rule(logger):
+    """添加脱敏规则"""
+    logger = logger.patch(mask_home_dir)
+    logger = logger.patch(mask_phone_num)
 
+    return logger
+
+logger = mask_rule(logger)
 
 # 全局异常捕获
 def handle_exception(exc_type, exc_value, exc_traceback):
