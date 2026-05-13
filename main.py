75a4da4bb88523b8752d5d85c21e2dd23bb4b0f0
狐日泽 <shdocter@nya-wsl.com>
2026-05-13 14:48:18 +0800

    适配hi168源新增的签名机制

--- main.py
+++ main.py
@@ -2381,7 +2381,7 @@ def index():
                 ui.notify("更新源为空，将尝试从Github获取更新", type="negative")
                 server = "https://github.com/Nya-WSL/bili_travail/releases/download/update/update.zip"
 
-            elif server in ["CN-QN", "hi168"]:
+            elif server in ["CN-QN"]:
                 server = f'{source.get("url", {}).get(server)}/{status}.zip'
 
             else:
