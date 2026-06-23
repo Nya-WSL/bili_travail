975e1259f3bdd4d93e8c2bd4440fe4d2c7c6d6f8
狐日泽 <shdocter@nya-wsl.com>
2026-06-23 16:47:41 +0800

    修复缺少字体文件时控制台报错的问题，现在会扫描字体文件并识别对应的类型

--- libs/styles.py
+++ libs/styles.py
@@ -1,21 +1,34 @@
 # third-party modules
 from nicegui import ui
+import os
+
+def check_font_file_exists():
+    formats = {
+        'ttf': 'truetype',
+        'woff2': 'woff2',
+        'woff': 'woff',
+    }
+
+    fonts = {}
+
+    for extension, font_format in formats.items():
+        if os.path.exists(f'static/fonts/custom-font.{extension}'):
+            fonts[extension] = font_format
+    return fonts
 
 def page_styles():
     ui.add_head_html(
-        """
+        f"""
         <style>
-        @font-face {
+        @font-face {{
             font-family: 'Custom Font';
-            src: url('/static/fonts/custom-font.woff2') format('woff2'),
-                url('/static/fonts/custom-font.woff') format('woff'),
-                url('/static/fonts/custom-font.ttf') format('truetype');
+            src: {", ".join(f"url('/static/fonts/custom-font.{extension}') format('{font_format}')" for extension, font_format in check_font_file_exists().items())};
             font-display: swap; /* 优化加载体验 */
-            }
+            }}
 
-        body {
+        body {{
             font-family: "Custom Font", sans-serif;
-            }
+            }}
         </style>
     """,
         shared=True,
