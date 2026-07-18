1ac464464b3069b89ce23c5f2a25405ae5c33d2c
狐日泽 <shdocter@nya-wsl.com>
2026-07-19 07:00:50 +0800

    修复获取房间礼物递归调用缺少return导致结果被丢弃的问题

--- libs/gift.py
+++ libs/gift.py
@@ -155,7 +155,7 @@ class BiliGiftManager:
                             self.wait_num += 1
                             logger.warning(f"房间{self.room_id}的自定义礼物数据可能还未下发，继续等待...")
                             await asyncio.sleep(5)
-                            await self.get_room_gift(platform)
+                            return await self.get_room_gift(platform)
                         else:
                             logger.info(f"成功获取房间{self.room_id}的礼物数据")
 
